83a0dc6e5e426790907a23cbb0ca9c4671d22c4e
colby <colbykrawczyk10@gmail.com>
2022-03-10 03:43:06 -0500

    Remove unused code

--- src/main/java/net/elytrapvp/elytratournament/listeners/BlockFromToListener.java
+++ src/main/java/net/elytrapvp/elytratournament/listeners/BlockFromToListener.java
@@ -1,7 +1,6 @@
 package net.elytrapvp.elytratournament.listeners;
 
 import net.elytrapvp.elytratournament.ElytraTournament;
-import net.elytrapvp.elytratournament.event.game.Game;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -12,13 +11,6 @@ import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class BlockFromToListener implements Listener {
-    private final ElytraTournament plugin;
-
-    public BlockFromToListener(ElytraTournament plugin) {
-        this.plugin = plugin;
-    }
-
-    /*
     private final ElytraTournament plugin;
 
     public BlockFromToListener(ElytraTournament plugin) {
@@ -55,59 +47,7 @@ public class BlockFromToListener implements Listener {
         for (BlockFace face : faces) {
             Block r = b.getRelative(face, 1);
             if (r.getType() == mirrorID1 || r.getType() == mirrorID2) {
-                Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                    r.setType(Material.AIR);
-                }, 200);
-                return true;
-            }
-        }
-        return false;
-    }
-
-     */
-
-    @EventHandler
-    public void onFromTo(BlockFromToEvent event)
-    {
-        int id = event.getBlock().getTypeId();
-        if(id >= 8 && id <= 11)
-        {
-            Block b = event.getToBlock();
-            int toid = b.getTypeId();
-            if(toid == 0)
-            {
-                if(generatesCobble(id, b))
-                {
-                    //event.setCancelled(true);
-                }
-            }
-        }
-    }
-
-    private final BlockFace[] faces = new BlockFace[]
-            {
-                    BlockFace.SELF,
-                    BlockFace.UP,
-                    BlockFace.DOWN,
-                    BlockFace.NORTH,
-                    BlockFace.EAST,
-                    BlockFace.SOUTH,
-                    BlockFace.WEST
-            };
-
-    public boolean generatesCobble(int id, Block b)
-    {
-        int mirrorID1 = (id == 8 || id == 9 ? 10 : 8);
-        int mirrorID2 = (id == 8 || id == 9 ? 11 : 9);
-        for(BlockFace face : faces)
-        {
-            Block r = b.getRelative(face, 1);
-            if(r.getTypeId() == mirrorID1 || r.getTypeId() == mirrorID2)
-            {
-                Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                    r.setType(Material.AIR);
-                }, 200);
-
+                Bukkit.getScheduler().runTaskLater(plugin, () -> r.setType(Material.AIR), 200);
 
                 for(BlockFace face2 : BlockFace.values()) {
                     Block s = r.getRelative(face2, 1);
@@ -115,14 +55,13 @@ public class BlockFromToListener implements Listener {
                     new BukkitRunnable() {
                         @Override
                         public void run() {
-                            if(s.getType() == Material.COBBLESTONE) {
+                            if (s.getType() == Material.COBBLESTONE) {
                                 // TODO: Check for both water and lava, and if found replace as lava.
                                 s.setType(Material.AIR);
                             }
                         }
                     }.runTaskLater(plugin, 200);
                 }
-
                 return true;
             }
         }
