bbaa8924035ca6c62719acc32f79001dcaa82163
colby <colbykrawczyk10@gmail.com>
2022-03-23 16:32:07 -0400

    Allow bowls to be dropped

--- src/main/java/net/elytrapvp/elytratournament/listeners/PlayerDropItemListener.java
+++ src/main/java/net/elytrapvp/elytratournament/listeners/PlayerDropItemListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 public class PlayerDropItemListener implements Listener {
     @EventHandler
     public void onDrop(PlayerDropItemEvent event) {
-        if(event.getItemDrop().getItemStack().getType() == Material.GLASS_BOTTLE) {
+        if(event.getItemDrop().getItemStack().getType() == Material.GLASS_BOTTLE || event.getItemDrop().getItemStack().getType() == Material.BOWL) {
             event.getItemDrop().remove();
             return;
         }
